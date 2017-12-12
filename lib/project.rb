afd7f58fc551f1cf60452119ebf81d2e2b6675f5
Hoon Lee <yl.hoony@gmail.com>
2017-12-12 18:29:53 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,14 @@
+class Project
+  attr_accessor :backers
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end
+end
